489e5face86c84b1b1af70bd1ab2dc59df7051c3
Aschonn <Aschonn_trinity@yahoo.com>
2020-10-08 14:32:24 -0500

    Changed Requir and contact

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-#sqlite
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+sqlite
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 #postgres
 
 # DATABASES = {
