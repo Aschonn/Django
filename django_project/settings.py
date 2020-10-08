554e0a77fb9442b8646a39db8b70cd0bb37e1cb9
Aschonn <Aschonn_trinity@yahoo.com>
2020-10-08 15:24:07 -0500

    changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # if debug is True else false
 DEBUG = (os.environ.get("DEBUG_VALUE") == 'True')
-ALLOWED_HOSTS = ['django-draft.herokuapp.com']
+ALLOWED_HOSTS = ['django-draft-blog.herokuapp.com']
 
 
 # Application definition
